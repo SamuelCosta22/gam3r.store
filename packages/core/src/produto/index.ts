a6d0d2f01cf5d733b2bff898ed8846f69c6c414a
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2024-10-06 10:49:03 -0300

    add new contexts and hooks in frontend

--- packages/core/src/produto/index.ts
+++ packages/core/src/produto/index.ts
@@ -1,5 +1,7 @@
 import Produto from "./Produto";
 import Especificacoes from "./Especificacoes";
 import Precificavel from "./Precificavel";
+import FiltrarProdutos from "./FiltrarProdutos";
 
 export type { Produto, Especificacoes, Precificavel }
+export { FiltrarProdutos }
